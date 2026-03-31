d4cd73126c61123bb362736e74addf402248e598
fernandoeduardolucas <fernandoeduardolucas@icloud.com>
2026-03-31 22:34:36 +0100

    Detalha CSV com índice, valor e peso dos itens

--- src/main/java/org/ant/ACOKnapsack.java
+++ src/main/java/org/ant/ACOKnapsack.java
@@ -114,9 +114,22 @@ public class ACOKnapsack {
         System.out.println("Melhor valor: " + melhor.valorTotal);
         System.out.println("Peso total: " + melhor.pesoTotal);
         System.out.print("Itens escolhidos (índices): ");
+        StringBuilder itensDetalhados = new StringBuilder();
         for (int i = 0; i < instancia.itens.length; i++) {
-            if (melhor.escolhidos[i]) System.out.print(i + " ");
+            if (melhor.escolhidos[i]) {
+                System.out.print(i + " ");
+                if (itensDetalhados.length() > 0) {
+                    itensDetalhados.append(" | ");
+                }
+                itensDetalhados
+                        .append(i)
+                        .append(":")
+                        .append(instancia.itens[i].valor)
+                        .append(":")
+                        .append(instancia.itens[i].peso);
+            }
         }
         System.out.println();
+        System.out.println("Itens escolhidos detalhados (indice:valor:peso): " + itensDetalhados);
     }
 }
